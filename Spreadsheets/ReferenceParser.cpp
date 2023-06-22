210457a223b4c40a9109420eaa863a4f71642a00
Todor Todorov <toshkofff19@gmail.com>
2023-06-22 04:55:41 +0300

    Commit

--- Spreadsheets/ReferenceParser.cpp
+++ Spreadsheets/ReferenceParser.cpp
@@ -1,35 +1,39 @@
 #include "ReferenceParser.h"
 
+void ExtractRCAt(size_t index, size_t length, const String& value, size_t& row, size_t& columns)
+{
+	String _row, _column;
+	size_t rowIndex = index + 1, columnIndex = index + 1;
+	for (size_t j = index + 1; j < length; j++)
+	{
+		if (value[j] == 'C')
+		{
+			columnIndex = j + 1;
+			_row = value.Substring(rowIndex, j - index);
+		}
+
+		if (!String::IsDigit(value[j]))
+			_column = value.Substring(columnIndex, columnIndex - j);
+	}
+}
+
 String ReferenceParser::SubstituteTalbeReferences(const String& value, const Vector<Vector<UniquePtr<Cell>>>& referenceTable)
 {
 	String parsedString;
 	size_t length = value.Length();
 	int row = -1, column = -1;
-	for (size_t i = 0; i < length; i++)
+	for (size_t i = 0; i < length;)
 	{
 		if (value[i] == 'R')
 		{
-			unsigned rowEndIndex = i;
-			for (size_t j = i + 1; j < length; j++)
-				if (value[j] == 'C')
-				{
-					rowEndIndex = j;
-					break;
-				}
-			row = value.Substring(i+1, rowEndIndex - i - 1).IntegerParse();
-			unsigned columnEndIndex = rowEndIndex;
-			for (size_t j = rowEndIndex + 1; j < length; j++)
-				if (!String::IsDigit(value[j]))
-				{
-					columnEndIndex = j;
-					break;
-				}
-			column = value.Substring(rowEndIndex + 1, columnEndIndex - rowEndIndex - 1).IntegerParse();
-			i = columnEndIndex;
+			size_t row, columns;
+			ExtractRCAt(i, length, value, row, columns);
 
-			parsedString += referenceTable[row][column]->ToString(referenceTable);
 		}
+		else
+			i++;
 		parsedString += value[i];
+
 	}
 	std::cout << parsedString << std::endl;
     return parsedString;
